eda88c59adc70d74b81739d064dc5e9c3cc91d66
dinosantic <dinos.mos@gmail.com>
2018-08-24 20:49:15 +0200

    sidenav for small screen

--- js/main.js
+++ js/main.js
@@ -12,4 +12,12 @@ $(document).ready(function(){
     });
 });
         
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+  }
 
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0px';
+    document.getElementById('main').style.marginLeft = '0px';
+  }
